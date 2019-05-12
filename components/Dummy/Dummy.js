e75c8ba37ebe65674b6905b68a4fd3b15e4ac997
Lee <lgdunkley@icloud.com>
2019-05-12 18:05:46 +0100

    add basic home page and styed-components dummy comp

---
+++ components/Dummy/Dummy.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Dummy = styled.div`
+	padding: 10px;
+	background: black;
+	color: white;
+`;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Dummy } from '../components/Dummy';
+
+const Home = () => {
+	return <Dummy>Welcome to Next.js!</Dummy>;
+};
+
+export default Home;
