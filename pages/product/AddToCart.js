09a029f7258307eed1c9d6ccc420d203988a35f3
Lee <lgdunkley@icloud.com>
2019-05-14 09:33:14 +0100

    add basic product details styles

--- pages/product/AddToCart.js
+++ pages/product/AddToCart.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+
+const PrimaryButton = styled.button`
+  background: #000000;
+  color: #ffffff;
+  text-transform: uppercase;
+  border: 0;
+  font-size: 16px;
+  padding: 16px;
+`;
+
+const QuantityControl = styled.button`
+  font-size: 12px;
+  background: #cccccc;
+  color: #666666;
+  height: 24px;
+  width: 24px;
+  text-align: center;
+  margin-bottom: 2px;
+  border: 0;
+`;
+
+const Quantity = styled.p`
+  font-size: 12px;
+  padding: 20px;
+  background: #eeeeee;
+  color: #666666;
+  height: 50px;
+  width: 50px;
+  text-align: center;
+  float: left;
+`;
+
+const Controls = styled.div`
+  float: left;
+  width: 24px;
+  margin-left: 2px;
+  margin-right: 20px;
+`;
 
 const addToCart = gql`
   mutation Increment($title: String!, $quantity: Int!) {
@@ -9,21 +48,32 @@ const addToCart = gql`
   }
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export const AddToCart = ({ title }) => {
   const [quantity, setQuantity] = React.useState(1);
   return (
-    <>
-      <button type="button" onClick={() => setQuantity(quantity + 1)}>
-        Increment
-      </button>
-      {quantity}
-      <button
-        type="button"
-        disabled={quantity === 1}
-        onClick={() => setQuantity(quantity - 1)}
-      >
-        Decrement
-      </button>
+    <Wrapper>
+      <Quantity>{quantity}</Quantity>
+      <Controls>
+        <QuantityControl
+          type="button"
+          onClick={() => setQuantity(quantity + 1)}
+        >
+          +
+        </QuantityControl>
+        <QuantityControl
+          type="button"
+          disabled={quantity === 1}
+          onClick={() => setQuantity(quantity - 1)}
+        >
+          -
+        </QuantityControl>
+      </Controls>
+
       <Mutation
         mutation={addToCart}
         awaitRefetchQueries
@@ -32,7 +82,7 @@ export const AddToCart = ({ title }) => {
         {(add, { loading, error }) => {
           return (
             <>
-              <button
+              <PrimaryButton
                 disabled={error || loading}
                 onClick={() => {
                   add({ variables: { title, quantity } });
@@ -40,11 +90,11 @@ export const AddToCart = ({ title }) => {
                 type="button"
               >
                 Add to cart
-              </button>
+              </PrimaryButton>
             </>
           );
         }}
       </Mutation>
-    </>
+    </Wrapper>
   );
 };
--- pages/product/ProductDetails.js
+++ pages/product/ProductDetails.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import { Row, Col } from 'styled-bootstrap-grid';
+import styled from 'styled-components';
 
 import { AddToCart } from './AddToCart';
 import { ResponsiveImage } from '../../components';
 
+const ProductImage = styled(ResponsiveImage)`
+  margin-bottom: 20px;
+`;
+
+const ProductInfo = styled.div`
+  text-align: center;
+  border-bottom: 1px solid #cccccc;
+  margin-bottom: 20px;
+`;
+
+const BrandTitle = styled.p`
+  font-size: 14px;
+  color: #666666;
+  margin-bottom: 20px;
+`;
+
+const ProductTitle = styled.p`
+  font-size: 28px;
+  line-height: 1.5;
+  color: #000000;
+  margin-bottom: 20px;
+  font-family: Georgia;
+`;
+
+const ProductPrice = styled.p`
+  font-size: 18px;
+  color: #666666;
+  margin-bottom: 20px;
+  font-family: Georgia;
+`;
+
+const ProductDescription = styled(BrandTitle)`
+  line-height: 1.5;
+`;
+
 export const ProductDetails = ({ title, brand, price, description, image }) => (
   <Row>
-    <Col xs="8" sm="8">
-      <ResponsiveImage src={`/static/${image}`} alt={title} />
+    <Col xs="12" md="6" lg="8">
+      <ProductImage src={`/static/${image}`} alt={title} />
     </Col>
-    <Col xs="4" sm="4">
-      <p>{brand}</p>
-      <p>{title}</p>
-      <p>{price}</p>
-      <p>{description}</p>
+    <Col xs="12" md="6" lg="4">
+      <ProductInfo>
+        <BrandTitle>{brand}</BrandTitle>
+        <ProductTitle>{title}</ProductTitle>
+        <ProductPrice>&#36;{price}</ProductPrice>
+        <ProductDescription>{description}</ProductDescription>
+      </ProductInfo>
+
       <AddToCart title={title} />
     </Col>
   </Row>
